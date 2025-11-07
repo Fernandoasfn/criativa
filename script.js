833620194e68c0b2faf9679f4d235a4746f8a1b9
Fernando Neves <fernando.asfn@gmail.com>
2025-11-07 10:02:10 -0400

    adicao ponto inicio caminho fotos

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 let fotosAniversario = [
-    '/imagens/aniversario/aniversario.png',
-    '/imagens/aniversario/aniversario2.png',
-    '/imagens/aniversario/aniversario3.png',
-    '/imagens/aniversario/aniversario4.png',
-    '/imagens/aniversario/aniversario5.png'
+    './imagens/aniversario/aniversario.png',
+    './imagens/aniversario/aniversario2.png',
+    './imagens/aniversario/aniversario3.png',
+    './imagens/aniversario/aniversario4.png',
+    './imagens/aniversario/aniversario5.png'
 ]
 
 let diversos = [
-    '/imagens/diversos/cerveja.png',
-    '/imagens/diversos/chapanela.png',
-    '/imagens/diversos/diversos.png',
-    '/imagens/diversos/diversos2.png',
-    '/imagens/diversos/diversos3.png',
-    '/imagens/diversos/tiktok.png'
+    './imagens/diversos/cerveja.png',
+    './imagens/diversos/chapanela.png',
+    './imagens/diversos/diversos.png',
+    './imagens/diversos/diversos2.png',
+    './imagens/diversos/diversos3.png',
+    './imagens/diversos/tiktok.png'
 ]
 
 let eventos = [
-    '/imagens/eventos/eventos.png',
-    '/imagens/eventos/eventos2.png',
-    '/imagens/eventos/eventos3.png',
-    '/imagens/eventos/eventos4.png',
-    '/imagens/eventos/eventos5.png',
-    '/imagens/eventos/eventos6.png'
+    './imagens/eventos/eventos.png',
+    './imagens/eventos/eventos2.png',
+    './imagens/eventos/eventos3.png',
+    './imagens/eventos/eventos4.png',
+    './imagens/eventos/eventos5.png',
+    './imagens/eventos/eventos6.png'
 ]
 
 let infantis = [
-    '/imagens/infantil/aniversario-menina.png',
-    '/imagens/infantil/aniversario-menina2.png',
-    '/imagens/infantil/infantil-menina.png',
-    '/imagens/infantil/infantil-menina2.png',
-    '/imagens/infantil/infantil-menina3.png',
-    '/imagens/infantil/infantil-menino.png',
-    '/imagens/infantil/infantil-menino2.png',
-    '/imagens/infantil/infantil-menino3.png',
-    '/imagens/infantil/infantil-menino4.png',
-    '/imagens/infantil/infantil-neutro.png',
-    '/imagens/infantil/infantil-neutro2.png',
-    '/imagens/infantil/infantil-neutro3.png'
+    './imagens/infantil/aniversario-menina.png',
+    './imagens/infantil/aniversario-menina2.png',
+    './imagens/infantil/infantil-menina.png',
+    './imagens/infantil/infantil-menina2.png',
+    './imagens/infantil/infantil-menina3.png',
+    './imagens/infantil/infantil-menino.png',
+    './imagens/infantil/infantil-menino2.png',
+    './imagens/infantil/infantil-menino3.png',
+    './imagens/infantil/infantil-menino4.png',
+    './imagens/infantil/infantil-neutro.png',
+    './imagens/infantil/infantil-neutro2.png',
+    './imagens/infantil/infantil-neutro3.png'
 ]
 
 let times = [
-    '/imagens/times/corinthians.png',
-    '/imagens/times/palmeiras.png',
-    '/imagens/times/vila.png'
+    './imagens/times/corinthians.png',
+    './imagens/times/palmeiras.png',
+    './imagens/times/vila.png'
 ]
 
 function gerarGaleria(titulo, arr) {
